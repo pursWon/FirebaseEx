455c856d01ee685a126fe26d7a2613625cc0b39f
WONGI HONG <dt95@naver.com>
2023-08-26 00:29:18 +0900

    ADD : 이미지를 Storage에서 다운받는 함수인 downloadImage를 생성 및 적용

--- FireBaseEx/ImageManageViewController.swift
+++ FireBaseEx/ImageManageViewController.swift
@@ -1,13 +1,12 @@
 import UIKit
 import FirebaseStorage
-import FirebaseAuth
 
 class ImageManageViewController: UIViewController {
     @IBOutlet weak var pictureImageView: UIImageView!
     @IBOutlet weak var storageSearchBar: UISearchBar!
     
     let storage = Storage.storage()
-    var imageNames: [String] = []
+    var storageItems: [StorageReference] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,30 +24,17 @@ class ImageManageViewController: UIViewController {
     func setUpstorageImage() {
         storage.reference().child("myImages").listAll { result, error in
             guard let items = result?.items else { return }
-            
-            items.forEach {
-                self.imageNames.append($0.name)
-            }
+            self.storageItems = items
         }
     }
     
-    @IBAction func imageUploadButton(_ sender: UIButton) {
-        guard let img = UIImage(named: "Van de ven") else { return }
-        var data = Data()
+    func downloadImage(text: String) {
+        let names = storageItems.map { $0.name.components(separatedBy: ".")[0] }
         
-        data = img.jpegData(compressionQuality: 0.8)! // 지정한 이미지를 포함하는 데이터 개체를 JPEG 형식으로 반환, 0.8은 데이터의 품질을 나타낸것 1에 가까울수록 품질이 높은 것
-        let filePath = "Van de ven"
-        let metaData = StorageMetadata() // Firebase 저장소에 있는 개체의 메타데이터를 나타내는 클래스, URL, 콘텐츠 유형 및 문제의 개체에 대한 FIRStorage 참조를 검색하는 데 사용
-        metaData.contentType = "image/png" // 데이터 타입을 image or png
-    }
-    
-    @IBAction func searchButton(_ sender: UIButton) {
-        guard let text = storageSearchBar.text else { return }
-        
-        if !imageNames.filter({ $0.components(separatedBy: ".")[0] == text }).isEmpty {
-            imageNames.forEach {
-                if $0.components(separatedBy: ".")[0] == text {
-                    storage.reference(forURL: "gs://fir-ex-41229.appspot.com/myImages/\($0)").downloadURL { url, error in
+        if names.contains(text) {
+            storageItems.forEach {
+                if $0.name.components(separatedBy: ".")[0] == text {
+                    storage.reference(forURL: "gs://fir-ex-41229.appspot.com/\($0.fullPath)").downloadURL { url, error in
                         guard error == nil else { return }
                         guard let url = url else { return }
                         
@@ -70,10 +56,26 @@ class ImageManageViewController: UIViewController {
             }
         }
     }
+    
+    @IBAction func imageUploadButton(_ sender: UIButton) {
+        guard let img = UIImage(named: "Van de ven") else { return }
+        var data = Data()
+        
+        data = img.jpegData(compressionQuality: 0.8)! // 지정한 이미지를 포함하는 데이터 개체를 JPEG 형식으로 반환, 0.8은 데이터의 품질을 나타낸것 1에 가까울수록 품질이 높은 것
+        let filePath = "Van de ven"
+        let metaData = StorageMetadata() // Firebase 저장소에 있는 개체의 메타데이터를 나타내는 클래스, URL, 콘텐츠 유형 및 문제의 개체에 대한 FIRStorage 참조를 검색하는 데 사용
+        metaData.contentType = "image/png" // 데이터 타입을 image or png
+    }
+    
+    @IBAction func searchButton(_ sender: UIButton) {
+        guard let searchText = storageSearchBar.text else { return }
+        downloadImage(text: searchText)
+    }
 }
 
 extension ImageManageViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard let text = storageSearchBar.text else { return }
+        guard let searchText = storageSearchBar.text else { return }
+        downloadImage(text: searchText)
     }
 }
