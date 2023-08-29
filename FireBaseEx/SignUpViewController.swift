92b96122a80e0611dcbdaa255d1b41238c77c551
WONGI HONG <dt95@naver.com>
2023-08-30 02:13:38 +0900

    ADD : 회원가입 여부에 따른 알림창 추가

--- FireBaseEx/SignUpViewController.swift
+++ FireBaseEx/SignUpViewController.swift
@@ -10,15 +10,29 @@ class SignUpViewController: UIViewController {
         
     }
     
+    func clearText() {
+        idTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
     @IBAction func signUpButtonClicked(_ sender: UIButton) {
-        guard let idText = idTextField.text else { return }
-        guard let passwordText = passwordTextField.text else { return }
+        guard
+            let idText = idTextField.text,
+            let passwordText = passwordTextField.text else { return }
         
         Auth.auth().createUser(withEmail: idText, password: passwordText) { authResult, error in
+            self.checkLogin(idText: idText, passwordText: passwordText)
+            
             if let error = error {
-                print(error.localizedDescription)
+                switch error.localizedDescription {
+                case "The email address is already in use by another account.":
+                    self.showAlert(title: "중복된 계정입니다.")
+                default:
+                    print(error.localizedDescription)
+                }
             } else {
-                print(authResult?.user.email)
+                self.showAlert(title: "회원가입이 완료되었습니다.")
+                self.clearText()
             }
         }
     }
