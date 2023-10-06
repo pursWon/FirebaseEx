bb70f4b148b9641c64f13369a3fc2660c4e10dfa
WONGI HONG <dt95@naver.com>
2023-10-06 23:02:11 +0900

    ADD : KingFisher 추가 및 적용

--- FireBaseEx/Alert/Alert.swift
+++ FireBaseEx/Alert/Alert.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Kingfisher
 
 extension UIViewController {
     func showAlert(title: String, message: String? = nil) {
@@ -8,5 +9,17 @@ extension UIViewController {
         
         present(alert, animated: true)
         return
+    }
+    
+    func showImage(title: String, name: String, imageURL: URL) {
+        let alert = UIAlertController(title: title, message: name, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        let imageView = UIImageView(frame: CGRect(x: 10, y: 10, width: 100, height: 100))
+        imageView.kf.setImage(with: imageURL)
+        
+        alert.view.addSubview(imageView)
+        alert.addAction(action)
+        
+        self.present(alert, animated: true)
     }
 }
