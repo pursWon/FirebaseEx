55a0e9c35e05604c21612b14c0e8f53072500cca
WONGI HONG <dt95@naver.com>
2023-10-13 23:43:21 +0900

    MOD : collectionView를 보여줄 데이터 배열 변경

--- FireBaseEx/ImagesViewController.swift
+++ FireBaseEx/ImagesViewController.swift
@@ -6,7 +6,7 @@ class ImagesViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
     let storage = Storage.storage()
-    var imageURLs: [URL] = []
+    var imagesInform: [ImageInform] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,20 +18,30 @@ class ImagesViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
     }
+    
+    @IBAction func moveButtonClicked(_ sender: UIButton) {
+        guard let imagesPageVC = self.storyboard?.instantiateViewController(withIdentifier: "ImagesPageViewController") as? ImagesPageViewController else { return }
+        imagesPageVC.imagesInform = imagesInform
+        
+        navigationController?.pushViewController(imagesPageVC, animated: true)
+    }
 }
 
 extension ImagesViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageURLs.count
+        return imagesInform.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImagesCell", for: indexPath) as?
                 ImagesCell else { return UICollectionViewCell() }
         
-        cell.myImageView.kf.setImage(with: imageURLs[indexPath.row])
+        cell.myImageView.kf.setImage(with: imagesInform[indexPath.row].url)
+        cell.nameLabel.text = imagesInform[indexPath.row].imageName
         cell.myImageView.contentMode = .scaleAspectFill
         cell.myImageView.clipsToBounds = true
+        cell.nameLabel.textAlignment = .center
+        cell.nameLabel.backgroundColor = .systemGray5
         
         return cell
     }
@@ -39,12 +49,10 @@ extension ImagesViewController: UICollectionViewDelegate, UICollectionViewDataSo
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = collectionView.frame.width
         let lineCount: CGFloat = 2
-        let interSpacing: CGFloat = 40
+        let interSpacing: CGFloat = 30
         let totalWidth = width - (interSpacing * (lineCount - 1))
         let itemSize: CGFloat = totalWidth / lineCount
         
         return CGSize(width: itemSize, height: itemSize)
     }
 }
-
-
