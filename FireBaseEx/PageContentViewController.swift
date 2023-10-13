761ae3d47c7bd994fd7e6b630a44ba1b94aea550
WONGI HONG <dt95@naver.com>
2023-10-13 23:43:46 +0900

    ADD  : Page VC의 내용을 담아줄 VC 추가

---
+++ FireBaseEx/PageContentViewController.swift
@@ -0,0 +1,52 @@
+import UIKit
+import SnapKit
+import Kingfisher
+
+class PageContentViewController: UIViewController {
+    private var myImageView = UIImageView()
+    private var nameLabel = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpUI()
+        setUpLayOut()
+    }
+    
+    init(imageURL: URL, imageName: String) {
+        super.init(nibName: nil, bundle: nil)
+        
+        myImageView.kf.setImage(with: imageURL)
+        nameLabel.text = imageName
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func setUpUI() {
+        view.backgroundColor = .white
+        myImageView.contentMode = .scaleAspectFill
+        nameLabel.textAlignment = .center
+    }
+    
+    private func setUpLayOut() {
+        view.addSubview(myImageView)
+        view.addSubview(nameLabel)
+        
+        myImageView.snp.makeConstraints { make in
+            make.leading.equalTo(50)
+            make.trailing.equalTo(-50)
+            make.top.equalTo(240)
+            make.bottom.equalTo(-240)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.leading.equalTo(70)
+            make.trailing.equalTo(-70)
+            make.top.equalTo(myImageView.snp.bottom).offset(10)
+            make.bottom.equalTo(-150)
+        }
+    }
+}
+
